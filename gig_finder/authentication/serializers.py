0a01b5a3a215b5ea3b6b3bb58618a848fe3509cb
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-10-08 13:35:55 -0400

    Fixed indentation for user serializer

--- gig_finder/authentication/serializers.py
+++ gig_finder/authentication/serializers.py
@@ -4,24 +4,20 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
 User = get_user_model()
-
 class RegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-
+    
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'is_musician' )
-
-        def create(self, validated_data):
-            user = User.objects.create(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                is_musician=validated_data['is_musician']
-            )
-
-            user.set_password(validated_data['password'])
-            user.save()
-
-            return user
+        email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+        password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            is_musician=validated_data['is_musician']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
